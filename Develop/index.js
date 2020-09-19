35405a504b649763d18e8b0b73538ccd6f21b127
Botticelli <alizehssn@gmail.com>
2020-09-18 19:15:27 -0500

    Question prompts and JSON packages installation

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,56 @@
 // array of questions for user
-const questions = [
+const questions = [{
+
+        type: "input",
+        content: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        content: "description",
+        message: "Describe your project?"
+    },
+    {
+        type: "input",
+        content: "tableOfContents",
+        message: "Create a table of contents?"
+    },
+    {
+        type: "input",
+        content: "installation",
+        message: "What are the steps required to install your project step-by-step?"
+    },
+    {
+        type: "input",
+        content: "usage",
+        message: "What are the instructions and examples for how to use your project?"
+    },
+    {
+        type: "input",
+        content: "contributing",
+        message: "Did you have any collaborators, if so what are their Github profiles?"
+
+    },
+    {
+        type: "input",
+        content: "license",
+        message: "Which license do you wish to include?"
+    },
+    {
+        type: "input",
+        content: "tests",
+        message: "What tests should be run for your application?"
+    },
+    {
+        type: "input",
+        content: "badges",
+        message: "Do you wish to include any badges?"
+    }
 
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
